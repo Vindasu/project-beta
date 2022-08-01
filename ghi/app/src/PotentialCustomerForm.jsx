ff05815836d3883a7bf39ef08ad132cfa93568f0
Alex <alexleeadams@aol.com>
2022-08-01 16:43:25 -0500

    updated form

--- ghi/app/src/PotentialCustomerForm.jsx
+++ ghi/app/src/PotentialCustomerForm.jsx
@@ -67,7 +67,7 @@ class PotentialCustomerForm extends React.Component {
         <div className="row">
             <div className="offset-3 col-6">
             <div className="shadow p-4 mt-4">
-                <h1>Create a new shoe</h1>
+                <h1>Create a new customer</h1>
                 <form onSubmit={this.handleSubmit} id="create-customer-form">
                 <div className="form-floating mb-3">
                     <input value={this.state.name} onChange={this.handleNameChange} placeholder="Name" required type="text" name="name" id="name" className="form-control" />
