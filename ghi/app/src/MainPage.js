86bf749e5f185b2842467242244e4559d9ad932d
Alex <alexleeadams@aol.com>
2022-08-02 03:28:51 -0500

    many hours gone to css

--- ghi/app/src/MainPage.js
+++ ghi/app/src/MainPage.js
@@ -4,8 +4,8 @@ function MainPage() {
     
     <div className="px-5 py-5 my-5 text-center">
     <div class="opacity-75">
-    <div class="p-1 mb-6 bg-dark text-white">
-    <div className="px-5 py-6 my-5 text-center">
+
+
     
     
     
@@ -13,15 +13,16 @@ function MainPage() {
       <h1>
         <img src="https://i.imgur.com/hXf7vQI.png" className="" alt= "CarCar Logo" width="150" height="150"/>
         </h1>
-        
+        <div class="container-sm">
       <div className="col-lg-3 mx-auto">
+      </div>
         <p className="lead mb-4">
           CarCar takes you FarFar... harhar
         </p>
       </div>
     </div>
-    </div>
-    </div>
+
+
   );
 }
 
--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -2,7 +2,7 @@ import { NavLink } from 'react-router-dom';
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-info">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
         
         <NavLink className="navbar-brand" to="/">CarCar</NavLink>
